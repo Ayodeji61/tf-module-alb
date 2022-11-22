e1955f3bf30839505416c50887fb3138f16a5950
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-21 17:38:03 -0700

    Updating

--- alb.tf
+++ alb.tf
@@ -41,6 +41,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_lb_listener" "main" {
+  count             = var.internal ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -55,3 +56,22 @@ resource "aws_lb_listener" "main" {
     }
   }
 }
+
+
+resource "aws_lb_listener" "public-http" {
+  count             = var.internal ? 0 : 1
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    fixed_response {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "200"
+    }
+  }
+}
+
