6dbfd5fb86c38f463c35e5d78a039bd28e9a193e
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-21 17:50:06 -0700

    Updating

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "lb_dns_name" {
 output "lb_listener_arn" {
   value = var.internal ? aws_lb_listener.main[0].arn : aws_lb_listener.public-http[0].arn
 }
+
+output "lb_arn" {
+  value = aws_lb.main.arn
+}
